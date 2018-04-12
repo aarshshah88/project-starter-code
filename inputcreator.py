021c5319a1d268caf8b51785a0130f588a78ebb9
Nicolas Budman <nicolasbudman@berkeley.edu>
2018-04-11 22:05:30 -0700

    create a tricky graph G that tricks greedy edge algorithms

--- inputcreator.py
+++ inputcreator.py
@@ -5,13 +5,11 @@ import numpy as np
 
 #takes in a graph and edge between u and v with weight x
 def edgeAdder(G, u, v, x):
-	edgeUtoVweightX = (u, v, x)
 	G.add_edge(u, v, weight= x)
-	return G
 
 #adding a node called,u , with conquesting cost, v, and edge degree, x. 
-def nodeAdder(G, u, v, x):
-	G.add_node(u, conquesting_cost = v, edge_degree = x)
+def nodeAdder(G, u, v):
+	G.add_node(u, conquesting_cost = v)
 
 
 #takes in a graph and writes it to a file
@@ -19,30 +17,29 @@ def write_to_file(G):
 	f = open("Inputs","w")
 	AL = nx.to_numpy_matrix(G)
 	print(AL)
-	adjacency_list_formatted =  []
 	list_of_data = G.nodes.data("conquesting_cost")
 	AL = AL.tolist()
+	print(type(AL))
+	count = 0
 	for n in G:
 		AL[n][n] = list_of_data[n]
-	
-			# start, end = a
-			# if start == x:
-
 	print(AL)
 
+
 G = nx.Graph()
-nodeAdder(G, 0, 3, 3)
-nodeAdder(G, 1, 4, 4)
-nodeAdder(G, 2, 5, 5)
-nodeAdder(G, 3, 6, 6)
-edgeAdder(G, 0, 1, 5)
-edgeAdder(G, 1, 2, 6)
-edgeAdder(G, 2, 3, 7)
+nodeAdder(G, 0, 20)
+nodeAdder(G, 1, 5)
+nodeAdder(G, 2, 30)
+nodeAdder(G, 3, 30)
+nodeAdder(G, 4, 30)
+edgeAdder(G, 0, 1, 20)
+edgeAdder(G, 0, 3, 5)
+edgeAdder(G, 1, 3, 10)
+edgeAdder(G, 1, 2, 10)
+edgeAdder(G, 1, 4, 10)
+edgeAdder(G, 2, 3, 10)
+edgeAdder(G, 3, 4, 10)
 
 write_to_file(G)
-
-
-
-
 
 
