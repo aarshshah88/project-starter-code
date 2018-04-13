2bd1c756f982e0586b94919e732bb6fc209c8aca
Aarsh Shah <aarshshah88@berkeley.edu>
2018-04-12 19:45:06 -0700

    nick + aarsh

--- inputcreator.py
+++ inputcreator.py
@@ -1,17 +1,16 @@
 import sys
 import networkx as nx
 import numpy as np
+import random
 
 
 #takes in a graph and edge between u and v with weight x
 def edgeAdder(G, u, v, x):
-	edgeUtoVweightX = (u, v, x)
 	G.add_edge(u, v, weight= x)
-	return G
 
-#adding a node called,u , with conquesting cost, v, and edge degree, x. 
-def nodeAdder(G, u, v, x):
-	G.add_node(u, conquesting_cost = v, edge_degree = x)
+#adding a node called,u , with conquesting cost, v
+def nodeAdder(G, u, v):
+	G.add_node(u, conquesting_cost = v)
 
 
 def adjaceny_matrix_creator(G):
@@ -61,19 +60,45 @@ def write_to_file(G):
 
 	return temp
 
-G = nx.Graph()
-list_of_edges = []
-nodeAdder(G, 0, 3, 3)
-nodeAdder(G, 1, 4, 4)
-nodeAdder(G, 2, 5, 5)
-nodeAdder(G, 3, 6, 6)
-edgeAdder(G, 0, 1, 2)
-edgeAdder(G, 1, 2, 6)
-edgeAdder(G, 2, 3, 7)
+def graphGenerator(n, k):
+	G = nx.Graph()
+	for i in np.arange(n):
+		r = random.randint(10, 100)
+		nodeAdder(G, i, r)
 
-temp = write_to_file(G)
+	for j in np.arange(k):
+		v = random.randint(0, n)
+		u = random.randint(0, n)
+		r = random.randint(10, 100)
+		if u != v:
+			edgeAdder(G, u, v, r)
 
+	AL = nx.to_numpy_matrix(G)
+	nx.draw_networkx(G)
+	return G
 
+#takes in a graph G and numbers n,k and creates a random graph around G with n vertices and k edges. Returns new graph H
+#def graphGenerator(G, n, k):
+	#H = G
+
+# G = nx.Graph()
+# nodeAdder(G, 0, 20)
+# nodeAdder(G, 1, 5)
+# nodeAdder(G, 2, 30)
+# nodeAdder(G, 3, 30)
+# nodeAdder(G, 4, 30)
+# edgeAdder(G, 0, 1, 20)
+# edgeAdder(G, 0, 3, 5)
+# edgeAdder(G, 1, 3, 10)
+# edgeAdder(G, 1, 2, 10)
+# edgeAdder(G, 1, 4, 10)
+# edgeAdder(G, 2, 3, 10)
+# edgeAdder(G, 3, 4, 10)
+
+# write_to_file(G)
+
+G=graphGenerator(50, 300)
+nx.write_graphml(G, 'testInputs')
 
 
 
